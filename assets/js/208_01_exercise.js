89687bf3fb31582c48078f30bffc1c0cc732af7d
Horayra077 <horayra5@gmail.com>
2023-01-15 04:29:58 +0600

    exercise

--- assets/js/208_01_exercise.js
+++ assets/js/208_01_exercise.js
@@ -1,13 +1,13 @@
 function capitalize(str) {
     let newStr = str.toLowerCase();
     let finalStr = newStr.replace(newStr[0], newStr[0].toUpperCase());
-    console.log(finalStr);
+    return finalStr;
 }
 
-capitalize('eggplant');
-capitalize('pamplemousse');
-capitalize('squid');
-capitalize('ABUHORAYRA');
+console.log(capitalize('eggplant'));
+console.log(capitalize('pamplemousse'));
+console.log(capitalize('squid'));
+console.log(capitalize('ABUHORAYRA'));
 
 
 console.log('/---------------------***------------------/');
@@ -36,3 +36,52 @@ console.log(returnDay(1));
 console.log(returnDay(7));
 console.log(returnDay(4));
 console.log(returnDay(0));
+
+
+console.log('/---------------------***------------------/');
+
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    }
+    return false;
+}
+
+console.log(isShortsWeather(80));
+console.log(isShortsWeather(48));
+console.log(isShortsWeather(75));
+
+console.log('/---------------------***------------------/');
+
+function lastElement(singleArray) {
+    let numbrLenght = singleArray.length;
+    return singleArray[numbrLenght - 1];
+}
+
+console.log(lastElement([3, 5, 7])); //7
+console.log(lastElement([1])); //1
+console.log(lastElement([])); //null
+
+console.log('/---------------------***------------------/');
+
+function multiply(x, y) {
+    return x * y;
+}
+
+console.log(multiply(2, 3)); // 6
+console.log(multiply(9, 9)); // 81
+console.log(multiply(5, 4)); // 20
+
+console.log('/---------------------***------------------/');
+
+function sumArray(arrayAccept) {
+    let sum = 0;
+    for (let i = 0; i < arrayAccept.length; i++) {
+        sum = sum + arrayAccept[i];
+    }
+    return sum;
+}
+
+console.log(sumArray([1, 2, 3])); // 6
+console.log(sumArray([2, 2, 2, 2])); // 8
+console.log(sumArray([50, 50, 1])); // 101
