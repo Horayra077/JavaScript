bf4649f0ed61ab2ad085fec4313473425fd21390
Horayra077 <horayra5@gmail.com>
2023-01-16 01:26:12 +0600

    Function Scope

---
+++ assets/js/210_Function_Scope.js
@@ -0,0 +1,37 @@
+// function collectEggs() {
+//     let totalEggs = 6;
+// }
+// collectEggs();
+// console.log(totalEggs); 
+
+// Uncaught ReferenceError: totalEggs is not defined 
+
+//------------------------//
+
+let totalEggs = 0;
+function collectEggs() {
+    totalEggs = 6;
+}
+console.log(totalEggs);
+collectEggs();
+console.log(totalEggs);
+
+//------------------------//
+
+let bird = 'Scarlet Macaw';
+function birdWatch() {
+    let bird = 'Great Blue Heron';
+}
+
+birdWatch()
+console.log(bird); // Scarlet Macaw
+
+//------------------------//
+
+let bestPlayer = 'Leo Messi';
+function goat() {
+    let bestPlayer = 'Diego Maradona';
+    console.log(bestPlayer);  // Diego Maradona
+}
+
+goat();
