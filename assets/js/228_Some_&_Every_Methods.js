f5982720306fc4bb566a77ce967ccd826c8645cb
Horayra077 <horayra5@gmail.com>
2023-01-27 00:03:37 +0600

    Every & some method

---
+++ assets/js/228_Some_&_Every_Methods.js
@@ -0,0 +1,65 @@
+// Every
+// test whether all elements in the array pass the provided functiuon.
+// It returns a Boolean value (true or false)
+
+const exams = [80, 98, 92, 78, 77, 90, 89, 84, 81, 77]
+
+const decideValue = exams.every(score => score >= 75)
+
+console.log(decideValue);
+
+
+// Some 
+// Similar to every, but returns ture if ANY of the array elements pass the test function
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99,
+        year: 1984
+    },
+    {
+        title: 'Sharknado',
+        score: 35,
+        year: 2013
+    },
+    {
+        title: '13 Going On 30',
+        score: 70,
+        year: 2004
+    },
+    {
+        title: 'Stand by Me',
+        score: 85,
+        year: 1986
+    },
+    {
+        title: 'Waterworld',
+        score: 62,
+        year: 1995
+    },
+    {
+        title: 'Jingle All The Way',
+        score: 71,
+        year: 1996
+    },
+    {
+        title: 'Parasite',
+        score: 95,
+        year: 2019
+    },
+    {
+        title: 'Notting Hill',
+        score: 77,
+        year: 1999
+    },
+    {
+        title: 'Alien',
+        score: 90,
+        year: 1979
+    }
+]
+
+const decideValue2 = movies.some(movie => movie.year > 2020)
+
+console.log(decideValue2);
