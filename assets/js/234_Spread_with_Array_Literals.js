721fb4a2b148f30628840c6d3cd470c7b8758806
Horayra077 <horayra5@gmail.com>
2023-02-05 03:12:21 +0600

    Spread with array literals

---
+++ assets/js/234_Spread_with_Array_Literals.js
@@ -0,0 +1,19 @@
+const cats = ['Blue', 'Scout', 'Rocket'];
+const dogs = ['Rusty', 'Wyatt'];
+
+const allCats = [...cats] // make a new copy of array
+
+console.log(cats);
+console.log(allCats);
+
+const allPets = [...cats, ...dogs]; // combine array ['Blue', 'Scout', 'Rocket', 'Rusty', 'Wyatt']
+
+console.log(allPets);
+
+const allPets2 = [...cats, ...dogs, 'burkman']; // combine array and add new element ['Blue', 'Scout', 'Rocket', 'Rusty', 'Wyatt', 'burkman'] 
+
+console.log(allPets2);
+
+const str = [...'hello']; // It also work on string
+
+console.log(str);
