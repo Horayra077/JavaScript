28811747a3db953dfd8e57d00ba2a36fe79ba0e3
Horayra077 <horayra5@gmail.com>
2023-02-17 00:22:41 +0600

    getComputedStyle()

--- assets/js/dom_224_to_253/248_Changing_Styles.js
+++ assets/js/dom_224_to_253/248_Changing_Styles.js
@@ -1,7 +1,6 @@
 const h1 = document.querySelector('h1');
 console.log(h1.style);
 h1.style.color = 'green';
-h1.style.fontSize = '3em';
 h1.style.border = '2px solid olive';
 
 //----------------------------------//
@@ -13,3 +12,13 @@ for (let link of allLink) {
     link.style.textDecorationColor = 'magenta';
     link.style.textDecorationStyle = 'wavy';
 }
+
+//------------------------------//
+
+console.log(h1.style.fontSize); // ''
+// So, In this way result not displayed
+// For this insted
+console.log(window.getComputedStyle(h1));
+console.log(window.getComputedStyle(h1).fontSize);
+console.log(window.getComputedStyle(h1).color);
+console.log(window.getComputedStyle(h1).fontFamily);
