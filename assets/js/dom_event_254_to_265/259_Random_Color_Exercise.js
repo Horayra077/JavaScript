3e1e08edb2d851034d85b388cc2631d27ce5ceef
Horayra077 <horayra5@gmail.com>
2023-03-05 23:53:32 +0600

    Random color exercise

---
+++ assets/js/dom_event_254_to_265/259_Random_Color_Exercise.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('btn');
+const h1 = document.getElementById('title');
+
+button.addEventListener('click', function () {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const newColor = `rgb(${r},${g},${b})`;
+    document.body.style.backgroundColor = newColor;
+    h1.innerHTML = newColor;
+})
